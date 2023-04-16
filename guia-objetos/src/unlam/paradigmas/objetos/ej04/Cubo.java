b4dc9990642f4bee8e37f181f9bccf97bec19d40
anabelacejas <anabela.cejas15@gmail.com>
2023-04-16 19:48:21 -0300

    add ejercicio 04

--- guia-objetos/src/unlam/paradigmas/objetos/ej04/Cubo.java
+++ guia-objetos/src/unlam/paradigmas/objetos/ej04/Cubo.java
@@ -2,61 +2,57 @@ package unlam.paradigmas.objetos.ej04;
 
 public class Cubo {
 
-	// completar
-	
-    /**
-     * pre : longitudLado es un valor mayor a 0.
-     * post: inicializa el cubo a partir de la longitud de lado dada
-     */
-    public Cubo (double longitudLado) {
-    	// completar
-    }
+	private double lado;
 
-    /**
-     * post: devuelve la longitud de todos los lados del cubo
-     */
-    public double obtenerLado() {
-    	// completar
-    	return -1;
-    }
-    
-    /**
-     * pre : longitudLado es un valor mayor a 0.
-     * post: cambia la longitud de todos los lados del cubo
-     */
-    public void cambiarLado(double longitudLado) {
-    	// completar
-    }
-    
-    /**
-     * post: devuelve el área de la superficie de las caras del cubo
-     */
-    public double obtenerAreaCara() {
-    	// completar
-    	return -1;
-    }
-    
-    /**
-     * pre: areaCara es un valor mayor a 0.
-     * post: cambia el área de las caras del cubo
-     */
-    public void cambiarAreaCara(double areaCara) {
-    	// completar
-    }
+	/**
+	 * pre : longitudLado es un valor mayor a 0. post: inicializa el cubo a partir
+	 * de la longitud de lado dada
+	 */
+	public Cubo(double longitudLado) {
+		this.lado = longitudLado;
+	}
 
-    /**
-     * post: devuelve el volumen que encierra el cubo
-     */
-    public double obtenerVolumen() {
-    	// completar
-    	return -1;
-    }
+	/**
+	 * post: devuelve la longitud de todos los lados del cubo
+	 */
+	public double obtenerLado() {
+		return this.lado;
+	}
 
-    /**
-     * pre: volumen es un valor mayor a 0.
-     * post: cambia el volumen del cubo
-     */
-    public void cambiarVolumen(double volumen) {
-    	// completar
-    }
+	/**
+	 * pre : longitudLado es un valor mayor a 0. post: cambia la longitud de todos
+	 * los lados del cubo
+	 */
+	public void cambiarLado(double longitudLado) {
+		this.lado = longitudLado;
+	}
+
+	/**
+	 * post: devuelve el área de la superficie de las caras del cubo
+	 */
+	public double obtenerAreaCara() {
+		return lado * lado;
+	}
+
+	/**
+	 * pre: areaCara es un valor mayor a 0. post: cambia el área de las caras del
+	 * cubo
+	 */
+	public void cambiarAreaCara(double areaCara) {
+		this.lado = areaCara / lado;
+	}
+
+	/**
+	 * post: devuelve el volumen que encierra el cubo
+	 */
+	public double obtenerVolumen() {
+		return Math.pow(lado, 3);
+	}
+
+	/**
+	 * pre: volumen es un valor mayor a 0. post: cambia el volumen del cubo
+	 */
+	public void cambiarVolumen(double volumen) {
+		this.lado = (volumen / lado)/ lado;
+	}
 }
